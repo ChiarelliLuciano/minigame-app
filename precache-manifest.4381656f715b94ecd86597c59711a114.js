9d5c50caf048c941ff725e7fc2a76662dd1445d6
Computadora <computadora@MacBook-Pro-de-Computadora.local>
2020-05-09 18:31:50 -0300

    Updates

---
+++ precache-manifest.4381656f715b94ecd86597c59711a114.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "442519df556c43bac1b7f89faf68d9fb",
+    "url": "/minigame-app/index.html"
+  },
+  {
+    "revision": "ab6af33a58bfc0d351b6",
+    "url": "/minigame-app/static/css/main.8319bff2.chunk.css"
+  },
+  {
+    "revision": "ba0647a871fabec03f49",
+    "url": "/minigame-app/static/js/2.643e59b0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/minigame-app/static/js/2.643e59b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab6af33a58bfc0d351b6",
+    "url": "/minigame-app/static/js/main.8c83f00f.chunk.js"
+  },
+  {
+    "revision": "0e404eb34045c73e59f7",
+    "url": "/minigame-app/static/js/runtime-main.2be24119.js"
+  }
+]);
