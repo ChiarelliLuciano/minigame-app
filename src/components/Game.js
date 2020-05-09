decb4e1fc8e4b7a8c005da95949db6497db498f8
Computadora <computadora@MacBook-Pro-de-Computadora.local>
2020-05-09 18:29:27 -0300

    first commit

---
+++ src/components/Game.js
@@ -0,0 +1,59 @@
+import React, { useState } from "react";
+import { calculateWinner } from "../helper";
+import Board from "./Board";
+import qr from "../qr.png";
+
+const Game = () => {
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const [stepNumber, setStepNumber] = useState(0);
+  const [xIsNext, setXisNext] = useState(true);
+  const winner = calculateWinner(history[stepNumber]);
+  const XO = xIsNext ? "X" : "O";
+
+  const handleClick = (i) => {
+    const historyPoint = history.slice(0, stepNumber + 1);
+    const current = historyPoint[stepNumber];
+    const squares = [...current];
+
+    if (winner || squares[i]) return;
+    squares[i] = XO;
+    setHistory([...historyPoint, squares]);
+    setStepNumber(historyPoint.length);
+    setXisNext(!xIsNext);
+  };
+
+  const jumpTo = (step) => {
+    setStepNumber(step);
+    setXisNext(step % 2 === 0);
+  };
+
+  const renderMoves = () =>
+    history.map((_step, move) => {
+      const destination = move ? `Ir al Movimiento #${move}` : "Ir al Comienzo";
+      return (
+        <li key={move}>
+          <button onClick={() => jumpTo(move)}>{destination}</button>
+        </li>
+      );
+    });
+
+  return (
+    <>
+      <h1>Mini Juego en React: Ta Te Ti</h1>
+      <Board squares={history[stepNumber]} onClick={handleClick} />
+      <div className="info-wrapper">
+        <div>
+          <h3>Historial:</h3>
+          {renderMoves()}
+        </div>
+        <h3>{winner ? "GANADOR : " + winner : "Turno de: " + XO}</h3>
+      </div>
+      <div>
+        <img style={{ marginLeft: "125px" }} src={qr} alt="QR"></img>
+        <h3 style={{ marginLeft: "110px" }}>Abre este juego en tu teléfono!</h3>
+      </div>
+    </>
+  );
+};
+
+export default Game;
